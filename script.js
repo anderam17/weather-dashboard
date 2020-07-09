628fb07497a0570b8e74f332f4f6febf6f17e224
Allana Anderson <allana.anderson17@gmail.com>
2020-07-09 09:22:14 -0600

    fixed bug that would not append my data to the page. Also added the weather icon to the city title.

--- script.js
+++ script.js
@@ -1,53 +1,52 @@
+var uvIndex = "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&appid=7457011ce05981da5d6a41314151e8a0"
 
-
-console.log("test")
 // ! Will need to get day from moment.js 
 
-// buildQuery: simply get api + key and put their city in it 
 
-//! Come back and check that the city is actually being added
+
 function buildQuery(){
 var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="
-var apiKey = "&appid=7457011ce05981da5d6a41314151e8a0"
+var apiKey = "&appid=7457011ce05981da5d6a41314151e8a0&units=imperial"
 
 var city = $("#city").val().trim();
 return queryURL + city + apiKey;
 }
 
-
-   
-   
-var uvIndex = "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&appid=7457011ce05981da5d6a41314151e8a0"
+// ! Will not append to page 
 function updatePage(weatherData) {
-    var i=0;
-    var day = weatherData.response.list[0];
-    var location = weatherData.response.city;
+    // var i=0;
+    var day = weatherData.list[0];
+    var location = weatherData.city;
+
+    console.log(weatherData);
 
     var city = $("#city").val().trim();
- // !This also needs the date from moment js.
-    var cityName = $("<h1>").text(city);
-    var dailyTemp = day.main.temp;
-    var dailyHumidity = day.main.humidity;
-    var windSpeed = day.wind.speed;
-    var lat = location.coord.lat;
-    var long = location.coord.long;
-    
-    $("#daily-forecast").append(cityName);
-    $("#daily-forecast").append("<p>Temperature: " + dailyTemp + "F</p>");
-    $("#daily-forecast").append("<p>Humidity: " + dailyHumidity + "%</p>");
-    $("#daily-forecast").append("<p>Wind Speed: " + windSpeed + "MPH</p>");
 
+ // !This also needs the date from moment js.
+
+    var lat = location.coord.lat;
+    var long = location.coord.lon;
+  
+    var card = $("<div class='card'>")
+    var cardBody = $("<div class='card-body'>")
+    
+    $(cardBody).append(`<h1>${city}<img src='https://openweathermap.org/img/w/${day.weather[0].icon}.png'></img></h1>`);
+    $(cardBody).append("<p>Temperature: " + day.main.temp + "F</p>");
+    $(cardBody).append("<p>Humidity: " + day.main.humidity + "%</p>");
+    $(cardBody).append("<p>Wind Speed: " + day.wind.speed + "MPH</p>");
 
     var uvIndex = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&appid=7457011ce05981da5d6a41314151e8a0"
-     
-        $.ajax({
-            url: uvIndex,
-            method: "GET"
-        }).then(function(response){
-            console.log(response);
-            var uv = response.daily[0].uvi;
-            $("#daily-forecast").append("<p>UV Index: " + uv + "</p>");
-        })
+    
+    $.ajax({
+        url: uvIndex,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        var uv = response.daily[0].uvi;
+        $(cardBody).append("<p>UV Index: " + uv + "</p>");
+    })
+    card.append(cardBody);
+    $("#daily-forecast").append(card);
 }
 
 
@@ -63,14 +62,14 @@ $("#submit-city").on("click", function(event) {
         clear();
       
         // Build the query URL for the ajax request to the NYT API
-        var queryURL = buildQueryURL();
+        var queryURL = buildQuery();
       
-        // Make the AJAX request to the API - GETs the JSON data at the queryURL.
         // The data then gets passed as an argument to the updatePage function
         $.ajax({
           url: queryURL,
           method: "GET"
-        }).then(updatePage);
+        }).then(updatePage)
+        ;
       });
 
 // update page: put new data into sections on the page
